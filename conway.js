cc78cb12a7de2b9fc4a7b2b4eac638bc68764e37
Sam Bao <sam.bao@cerner.com>
2013-03-17 23:01:41 -0500

    Reswizzle and documented

--- conway.js
+++ conway.js
@@ -1,30 +1,51 @@
 /*
+Conway's Game of Life.
+Default board size is 25x25 or you can passing an array to specify the dimensions of the board.
+
+e.g.
 c = new Conway([3,3])
-c.setBoard([ [ true, false, false ],
-     [ true, true, false ],
-     [ false, true, true ] ])
- */
 
+You can randomly generate the board:
+c.populateBoard()
+
+or you can set the board explicitly:
+c.setBoard([[ true, false, false ],
+            [ true, true, false ],
+            [ false, true, true ]])
+
+To progress the board to the next generation
+c.nextGeneration()
+*/
 function Conway(dimensions) {
-  /* public variables */
-  this.board = []
-
-  /* private variables */
+  var board = []
   var dimensions = typeof dimensions !== 'undefined' ? dimensions : [25,25]
   var neighborRanges = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
-  /* public methods */
+  /* Randomly generates the board */
   this.populateBoard = function() {
-    this.board = populateBoard(dimensions[0], dimensions[1])
-    printBoard(this.board)
+    board = populateBoard(dimensions[0], dimensions[1])
+    this.printBoard(board)
   }
 
+  /* Set the board explicitly so we can do repeat runs
+   * newBoard - 2d array of booleans at indexes
+   * e.g. [[ true, false, false ],
+   *       [ true, true, false ],
+   *       [ false, true, true ]]
+   */
+  this.setBoard = function(newBoard) {
+    board = newBoard
+    this.printBoard(newBoard)
+  }
+
+  /* Progress the board to the next generation */
   this.nextGeneration = function () {
-    var newBoard = deepCopy(this.board)
+    var newBoard = new Array(dimensions[0])
     for (var x = 0; x < dimensions[0]; x++) {
+      newBoard[x] = new Array(dimensions[1])
       for (var y = 0; y < dimensions[1]; y++) {
-        var n = aliveNeighborCount(this.board, x, y)
-        var alive = this.board[x][y]
+        var n = aliveNeighborCount(x, y)
+        var alive = board[x][y]
         if (alive) {
           if (n === 2 || n === 3) {
             newBoard[x][y] = true
@@ -40,16 +61,12 @@ function Conway(dimensions) {
         }
       }
     }
-    this.board = newBoard
-    printBoard(this.board)
+    board = newBoard
+    this.printBoard()
   }
 
-  this.setBoard = function(board) {
-    this.board = board
-    printBoard(this.board)
-  }
-
-  var printBoard = function(board) {
+  /* Displays the current board in ascii */
+  this.printBoard = function() {
     for (var i = 0, bl = board.length; i < bl; i++) {
       var printString = ["|"]
       var row = board[i]
@@ -65,26 +82,8 @@ function Conway(dimensions) {
     }
   }
 
-  /* http://james.padolsey.com/javascript/deep-copying-of-objects-and-arrays/ */
-  var deepCopy = function (obj) {
-    if (Object.prototype.toString.call(obj) === '[object Array]') {
-        var out = [], i = 0, len = obj.length;
-        for ( ; i < len; i++ ) {
-            out[i] = arguments.callee(obj[i]);
-        }
-        return out;
-    }
-    if (typeof obj === 'object') {
-        var out = {}, i;
-        for ( i in obj ) {
-            out[i] = arguments.callee(obj[i]);
-        }
-        return out;
-    }
-    return obj;
-}
-
-  var aliveNeighborCount = function(board, x, y) {
+  /* private - returns the number of live neighbors for the given x,y */
+  var aliveNeighborCount = function(x, y) {
     var n = 0;
     var neighborsCoords = neighbors(x, y)
     for (var i = 0, ncl = neighborsCoords.length; i < ncl; i++) {
@@ -96,6 +95,10 @@ function Conway(dimensions) {
     return n;
   }
 
+  /* private - return a list of neighbor coordinates for the given x, y
+   * with in the boundaries of the board.
+   * e.g. for [0,0] returns [[0,1],[1,0],[1,1]]
+   */
   var neighbors = function(x, y) {
     var neighborsCoords = []
     for (var i = 0, nrl = neighborRanges.length; i < nrl; i++) {
@@ -108,6 +111,9 @@ function Conway(dimensions) {
     return neighborsCoords
   }
 
+  /* private - returns a boolean indicating whether the given x, y
+   * falls within the boundaries of board
+   */
   var withInDimensions = function (x, y) {
     if ((x >= 0 && x < dimensions[0]) && (y >= 0 && y < dimensions[1])) {
       return true
@@ -115,7 +121,7 @@ function Conway(dimensions) {
     return false
   }
 
-  /* private methods */
+  /* private - returns a randomly generated board for the number of given rows and columns */
   var populateBoard = function(rows, columns) {
     var bools = [true,false]
     var board = []
